b71ad6a446ed021b82d2e743d65951609f5b1c15
try.lopi <55300886+settom@users.noreply.github.com>
2019-09-22 14:00:30 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,162 @@
+        if(args[0].toLowerCase() == "leavea"){
+          var msg = '!leave'
+          if(args[1].toLowerCase() == "1"){
+            eg.communicator.sendMessage(DANG1172s_BOT1_ID, msg);
+          }
+          if(args[1].toLowerCase() == "2"){
+            eg.communicator.sendMessage(DANG1172s_BOT1_ID, msg);
+            eg.communicator.sendMessage(DANG1172s_BOT2_ID, msg);
+          }
+          if(args[1].toLowerCase() == "3"){
+            eg.communicator.sendMessage(DANG1172s_BOT1_ID, msg);
+            eg.communicator.sendMessage(DANG1172s_BOT2_ID, msg);
+            eg.communicator.sendMessage(DANG1172s_BOT3_ID, msg);
+          }
+          if(args[1].toLowerCase() == "4"){
+            eg.communicator.sendMessage(DANG1172s_BOT1_ID, msg);
+            eg.communicator.sendMessage(DANG1172s_BOT2_ID, msg);
+            eg.communicator.sendMessage(DANG1172s_BOT3_ID, msg);
+            eg.communicator.sendMessage(DANG1172s_BOT4_ID, msg);
+          }
+          if(args[1].toLowerCase() == "5"){
+            eg.communicator.sendMessage(DANG1172s_BOT1_ID, msg);
+            eg.communicator.sendMessage(DANG1172s_BOT2_ID, msg);
+            eg.communicator.sendMessage(DANG1172s_BOT3_ID, msg);
+            eg.communicator.sendMessage(DANG1172s_BOT4_ID, msg);
+            eg.communicator.sendMessage(DANG1172s_BOT5_ID, msg);
+          }
+          if(args[1].toLowerCase() == "6"){
+            eg.communicator.sendMessage(DANG1172s_BOT1_ID, msg);
+            eg.communicator.sendMessage(DANG1172s_BOT2_ID, msg);
+            eg.communicator.sendMessage(DANG1172s_BOT3_ID, msg);
+            eg.communicator.sendMessage(DANG1172s_BOT4_ID, msg);
+            eg.communicator.sendMessage(DANG1172s_BOT5_ID, msg);
+            eg.communicator.sendMessage(DANG1172s_BOT6_ID, msg);
+          }
+          if(args[1].toLowerCase() == "7"){
+            eg.communicator.sendMessage(DANG1172s_BOT1_ID, msg);
+            eg.communicator.sendMessage(DANG1172s_BOT2_ID, msg);
+            eg.communicator.sendMessage(DANG1172s_BOT3_ID, msg);
+            eg.communicator.sendMessage(DANG1172s_BOT4_ID, msg);
+            eg.communicator.sendMessage(DANG1172s_BOT5_ID, msg);
+            eg.communicator.sendMessage(DANG1172s_BOT6_ID, msg);
+            eg.communicator.sendMessage(DANG1172s_BOT7_ID, msg);
+          }
+          if(args[1].toLowerCase() == "8"){
+            eg.communicator.sendMessage(DANG1172s_BOT1_ID, msg);
+            eg.communicator.sendMessage(DANG1172s_BOT2_ID, msg);
+            eg.communicator.sendMessage(DANG1172s_BOT3_ID, msg);
+            eg.communicator.sendMessage(DANG1172s_BOT4_ID, msg);
+            eg.communicator.sendMessage(DANG1172s_BOT5_ID, msg);
+            eg.communicator.sendMessage(DANG1172s_BOT6_ID, msg);
+            eg.communicator.sendMessage(DANG1172s_BOT7_ID, msg);
+            eg.communicator.sendMessage(DANG1172s_BOT8_ID, msg);
+          }
+          if(args[1].toLowerCase() == "9"){
+            eg.communicator.sendMessage(DANG1172s_BOT1_ID, msg);
+            eg.communicator.sendMessage(DANG1172s_BOT2_ID, msg);
+            eg.communicator.sendMessage(DANG1172s_BOT3_ID, msg);
+            eg.communicator.sendMessage(DANG1172s_BOT4_ID, msg);
+            eg.communicator.sendMessage(DANG1172s_BOT5_ID, msg);
+            eg.communicator.sendMessage(DANG1172s_BOT6_ID, msg);
+            eg.communicator.sendMessage(DANG1172s_BOT7_ID, msg);
+            eg.communicator.sendMessage(DANG1172s_BOT8_ID, msg);
+            eg.communicator.sendMessage(DANG1172s_BOT9_ID, msg);
+          }
+          if(args[1].toLowerCase() == "10"){
+            eg.communicator.sendMessage(DANG1172s_BOT1_ID, msg);
+            eg.communicator.sendMessage(DANG1172s_BOT2_ID, msg);
+            eg.communicator.sendMessage(DANG1172s_BOT3_ID, msg);
+            eg.communicator.sendMessage(DANG1172s_BOT4_ID, msg);
+            eg.communicator.sendMessage(DANG1172s_BOT5_ID, msg);
+            eg.communicator.sendMessage(DANG1172s_BOT6_ID, msg);
+            eg.communicator.sendMessage(DANG1172s_BOT7_ID, msg);
+            eg.communicator.sendMessage(DANG1172s_BOT8_ID, msg);
+            eg.communicator.sendMessage(DANG1172s_BOT9_ID, msg);
+            eg.communicator.sendMessage(DANG1172s_BOT10_ID, msg);
+          }
+          if(args[1].toLowerCase() == "11"){
+            eg.communicator.sendMessage(DANG1172s_BOT1_ID, msg);
+            eg.communicator.sendMessage(DANG1172s_BOT2_ID, msg);
+            eg.communicator.sendMessage(DANG1172s_BOT3_ID, msg);
+            eg.communicator.sendMessage(DANG1172s_BOT4_ID, msg);
+            eg.communicator.sendMessage(DANG1172s_BOT5_ID, msg);
+            eg.communicator.sendMessage(DANG1172s_BOT6_ID, msg);
+            eg.communicator.sendMessage(DANG1172s_BOT7_ID, msg);
+            eg.communicator.sendMessage(DANG1172s_BOT8_ID, msg);
+            eg.communicator.sendMessage(DANG1172s_BOT9_ID, msg);
+            eg.communicator.sendMessage(DANG1172s_BOT10_ID, msg);
+            eg.communicator.sendMessage(DANG1172s_BOT11_ID, msg);
+          }
+          if(args[1].toLowerCase() == "12"){
+            eg.communicator.sendMessage(DANG1172s_BOT1_ID, msg);
+            eg.communicator.sendMessage(DANG1172s_BOT2_ID, msg);
+            eg.communicator.sendMessage(DANG1172s_BOT3_ID, msg);
+            eg.communicator.sendMessage(DANG1172s_BOT4_ID, msg);
+            eg.communicator.sendMessage(DANG1172s_BOT5_ID, msg);
+            eg.communicator.sendMessage(DANG1172s_BOT6_ID, msg);
+            eg.communicator.sendMessage(DANG1172s_BOT7_ID, msg);
+            eg.communicator.sendMessage(DANG1172s_BOT8_ID, msg);
+            eg.communicator.sendMessage(DANG1172s_BOT9_ID, msg);
+            eg.communicator.sendMessage(DANG1172s_BOT10_ID, msg);
+            eg.communicator.sendMessage(DANG1172s_BOT11_ID, msg);
+            eg.communicator.sendMessage(DANG1172s_BOT12_ID, msg);
+          }
+          if(args[1].toLowerCase() == "13"){
+            eg.communicator.sendMessage(DANG1172s_BOT1_ID, msg);
+            eg.communicator.sendMessage(DANG1172s_BOT2_ID, msg);
+            eg.communicator.sendMessage(DANG1172s_BOT3_ID, msg);
+            eg.communicator.sendMessage(DANG1172s_BOT4_ID, msg);
+            eg.communicator.sendMessage(DANG1172s_BOT5_ID, msg);
+            eg.communicator.sendMessage(DANG1172s_BOT6_ID, msg);
+            eg.communicator.sendMessage(DANG1172s_BOT7_ID, msg);
+            eg.communicator.sendMessage(DANG1172s_BOT8_ID, msg);
+            eg.communicator.sendMessage(DANG1172s_BOT9_ID, msg);
+            eg.communicator.sendMessage(DANG1172s_BOT10_ID, msg);
+            eg.communicator.sendMessage(DANG1172s_BOT11_ID, msg);
+            eg.communicator.sendMessage(DANG1172s_BOT12_ID, msg);
+            eg.communicator.sendMessage(DANG1172s_BOT13_ID, msg);
+          }
+          if(args[1].toLowerCase() == "14"){
+            eg.communicator.sendMessage(DANG1172s_BOT1_ID, msg);
+            eg.communicator.sendMessage(DANG1172s_BOT2_ID, msg);
+            eg.communicator.sendMessage(DANG1172s_BOT3_ID, msg);
+            eg.communicator.sendMessage(DANG1172s_BOT4_ID, msg);
+            eg.communicator.sendMessage(DANG1172s_BOT5_ID, msg);
+            eg.communicator.sendMessage(DANG1172s_BOT6_ID, msg);
+            eg.communicator.sendMessage(DANG1172s_BOT7_ID, msg);
+            eg.communicator.sendMessage(DANG1172s_BOT8_ID, msg);
+            eg.communicator.sendMessage(DANG1172s_BOT9_ID, msg);
+            eg.communicator.sendMessage(DANG1172s_BOT10_ID, msg);
+            eg.communicator.sendMessage(DANG1172s_BOT11_ID, msg);
+            eg.communicator.sendMessage(DANG1172s_BOT12_ID, msg);
+            eg.communicator.sendMessage(DANG1172s_BOT13_ID, msg);
+            eg.communicator.sendMessage(DANG1172s_BOT14_ID, msg);
+          }
+          if(args[1].toLowerCase() == "15"){
+            var msg = "!leave"
+            eg.communicator.sendMessage(DANG1172s_BOT_ID, msg);
+            eg.communicator.sendMessage(DANG1172s_BOT1_ID, msg);
+              eg.communicator.sendMessage(DANG1172s_BOT2_ID, msg);
+              eg.communicator.sendMessage(DANG1172s_BOT3_ID, msg);
+              eg.communicator.sendMessage(DANG1172s_BOT4_ID, msg);
+              eg.communicator.sendMessage(DANG1172s_BOT5_ID, msg);
+              eg.communicator.sendMessage(DANG1172s_BOT6_ID, msg);
+              eg.communicator.sendMessage(DANG1172s_BOT7_ID, msg);
+              eg.communicator.sendMessage(DANG1172s_BOT8_ID, msg);
+              eg.communicator.sendMessage(DANG1172s_BOT9_ID, msg);
+              eg.communicator.sendMessage(DANG1172s_BOT10_ID, msg);
+              eg.communicator.sendMessage(DANG1172s_BOT11_ID, msg);
+              eg.communicator.sendMessage(DANG1172s_BOT12_ID, msg);
+              eg.communicator.sendMessage(DANG1172s_BOT13_ID, msg);
+              eg.communicator.sendMessage(DANG1172s_BOT14_ID, msg);
+              eg.communicator.sendMessage(DANG1172s_BOT15_ID, msg);
+          }
+          if(args[1].toLowerCase() == "bot"){
+            fortnite.party.leave();
+            eg.communicator.sendMessage(data.friend.id, "The bot you are using has left!");
+          }
+          else{
+            eg.communicator.sendMessage(data.friend.id, "Please use !ready ON/OFF");
+          }
+      }
